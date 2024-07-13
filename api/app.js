d6c0b29c887ab41fbba9dfd5ed7feb446fc0773b
Danifanton <danielrxd0912@gmail.com>
2024-07-13 17:10:11 -0400

    Create middlewares

--- api/app.js
+++ api/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const cors = require('cors');
-const pool = require('pg'); // this is for vercel to work
+const cors = require('cors')
+const pool = require('pg');  // this is for vercel to work
+
+const routerApi = require('./routes');
+const { errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+const setupAuthStrategies = require('./utils/auth');
+
+// const https = require('https');
 
 function createApp() {
   const app = express();
@@ -8,9 +14,17 @@ function createApp() {
   app.use(express.json());
   app.use(cors());
 
+  setupAuthStrategies();
+
   app.get('/', (req, res) => {
     res.send('Hola mi server en express');
   });
+
+  routerApi(app);
+
+  app.use(ormErrorHandler);
+  app.use(boomErrorHandler);
+  app.use(errorHandler);
 
   return app;
 }
--- api/utils/auth/index.js
+++ api/utils/auth/index.js
@@ -1,7 +1,11 @@
-const passport = require("passport");
-const localStrategy = require("./strategies/local.strategy");
-const JwtStrategy = require("./strategies/jwt.strategy");
+const passport = require('passport');
 
-passport.use(localStrategy);
-passport.use(JwtStrategy);
+const LocalStrategy = require('./strategies/local.strategy');
+const JwtStrategy = require('./strategies/jwt.strategy');
 
+function setupAuthStrategies(){
+  passport.use(LocalStrategy);
+  passport.use(JwtStrategy);
+}
+
+module.exports = setupAuthStrategies;
